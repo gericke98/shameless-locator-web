9e9e3fc625da678e28ce2ec4a0e9401323079ef5
gericke98 <sgerickee@gmail.com>
2024-07-19 21:53:40 +0200

    vamo vamooo

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+  },
 };
 
 export default nextConfig;
