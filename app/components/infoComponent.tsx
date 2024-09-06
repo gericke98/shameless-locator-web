22a5e147130cbdc9b7d37876121f20b6936d9baf
gericke98 <sgerickee@gmail.com>
2024-09-06 14:30:47 +0200

    ui changes

--- app/components/infoComponent.tsx
+++ app/components/infoComponent.tsx
@@ -43,7 +43,7 @@ export const InfoComponent = ({ order }: Props) => {
           <h5 className="lg:text-base text-xs pl-14">
             {order.shipping.phone || "No information provided"}
           </h5>
-          <p className="text-sm text-black -pl-10 pt-5 items-center text-wrap w-full">
+          <p className="lg:text-sm text-xs text-black -pl-10 pt-5 items-center text-wrap w-full">
             Para cualquier modificación del envío, contactar con el
             <span className="font-bold"> +34 916 31 67 12</span> indicando el
             número localizador
